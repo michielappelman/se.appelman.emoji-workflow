94abec3012ef071ff973f271594a657765fbfe4f
Michiel Appelman <michiel@appelman.se>
2017-07-17 16:27:54 +0200

    Adjusts styling just a little bit

--- emoji-search.py
+++ emoji-search.py
@@ -18,7 +18,9 @@ for emoji_name, emoji_description in emoji_json.items():
     if len(list(top)) > 24:
         continue
     if search_term in emoji_name and emoji_description['char']:
-        item = SubElement(top, 'item', {'uid': emoji_name, 'arg': emoji_description['char']})
+        item = SubElement(
+            top, 'item', {'uid': emoji_name,
+                          'arg': emoji_description['char']})
         icon = SubElement(item, 'icon')
         icon.text = 'icon.png'
         title = SubElement(item, 'title')
@@ -27,7 +29,9 @@ for emoji_name, emoji_description in emoji_json.items():
         subtitle.text = "Paste \'{}\'  at cursor.".format(emoji_name)
     for keyword in emoji_description['keywords']:
         if search_term in keyword and emoji_description['char']:
-            item = SubElement(top, 'item', {'uid': emoji_name, 'arg': emoji_description['char']})
+            item = SubElement(top, 'item', {
+                'uid': emoji_name,
+                'arg': emoji_description['char']})
             icon = SubElement(item, 'icon')
             icon.text = 'icon.png'
             title = SubElement(item, 'title')
@@ -36,12 +40,12 @@ for emoji_name, emoji_description in emoji_json.items():
             subtitle.text = "Paste \'{}\'  at cursor.".format(emoji_name)
 
 if len(list(top)) == 0:
-        item = SubElement(top, 'item', {'uid': 'missing', 'arg': 'missing'})
-        icon = SubElement(item, 'icon')
-        icon.text = 'frowning.png'
-        title = SubElement(item, 'title')
-        title.text = u"Nope... 🙁"
-        subtitle = SubElement(item, 'subtitle')
-        subtitle.text = "No matching emoji found..."
+    item = SubElement(top, 'item', {'uid': 'missing', 'arg': 'missing'})
+    icon = SubElement(item, 'icon')
+    icon.text = 'frowning.png'
+    title = SubElement(item, 'title')
+    title.text = u"Nope... 🙁"
+    subtitle = SubElement(item, 'subtitle')
+    subtitle.text = "No matching emoji found..."
 
 print(tostring(top, encoding="utf8"))
